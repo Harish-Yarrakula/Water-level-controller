3162b652ffec7580456e7e02f2d598c2ce0d6c53
Harish-Yarrakula <yarrakulakusumaharish@gmail.com>
2025-06-21 08:17:59 +0530

    Update WifiToggle.jsx

--- frontend/src/componets/WifiToggle.jsx
+++ frontend/src/componets/WifiToggle.jsx
@@ -19,7 +19,7 @@ const WifiToggle = () => {
     React.  useEffect(() => {
         const fetchData = async () => {
           try {
-            const res = await fetch(`${BASE_URL}/api/analytics`);
+            const res = await fetch(`https://water-level-controller.onrender.com/api/analytics`);
             const json = await res.json();
             setData(json);
             updateValues(json);
